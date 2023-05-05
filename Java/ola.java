9caba50f16fffe0ebfa374452f8c64886dee14e8
gustavo17042 <130669211+gustavo17042@users.noreply.github.com>
2023-05-05 13:55:44 -0300

    first commit

---
+++ Java/ola.java
@@ -0,0 +1,46 @@
+import javax.swing.plaf.OptionPaneUI;
+
+public class Ola{
+    /**
+     * @param args
+     */
+    public static void main(string[] args){
+    system.out.println("boa tarde a todos!");
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    OptionPane.
+    }
+}
